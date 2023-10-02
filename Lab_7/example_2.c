a137f15e23705258f9891699429f7ff21b5bd9f9
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-02 15:59:30 +0500

    hello

---
+++ Lab_7/example_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    printf("Enter th digit ..");
+    scanf("%d",&a);
+if (a>=0){  if (a<=100){printf("enterED  a valid number %d",a);}
+            else{printf("entered value is greater then 100");}}
+else {printf("INVALID");}
+
+  return 0;
+}
