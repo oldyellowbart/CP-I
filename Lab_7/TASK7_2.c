170794f3c571461c0328fcf8cf3c5e6e89f22f79
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-03 14:43:49 +0500

    dfdf

---
+++ Lab_7/TASK7_2.c
@@ -0,0 +1,19 @@
+/*
+ -------------------------------------------------------------------------------- 
+  Author  :  Haseeb-ul-hassan
+  Roll No :  2022MC58
+  Dated   :  03/10/23
+  Purpose :  To get an area of triangle
+  -------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a five digit number >>");
+    scanf("%d",&a);
+        if (a>9999){if(a<=99999){printf("You entered a  5 digit Number %d",a);}
+                   else{printf("Enter a Valid 5 digit Number");}}
+        else{printf("Enter a Valid 5 digit Number");}
+    return 0;
+
+}
