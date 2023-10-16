5e3e1d4d051b0ade475338689ca945650ec46217
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-16 16:08:03 +0500

    nothing

--- Lab_9/task_5.c
+++ Lab_9/task_5.c
@@ -3,7 +3,7 @@
   Roll No       :  2022MC58
   Dated         :  16/10/23
   Purpose       :  Task 9_5 to get know about the loops
-                    and print the factorial  of given number
+                   and print the factorial  of given number
   Disclaimer    :  open source Code has been writen for educational purpose only 
   -------------------------------------------------------------------------------*/
 #include<stdio.h>
@@ -17,7 +17,7 @@ void main()
 }
 void fact(int d)
 {
-    int c=0,f=1,g=0;
+    int c=0,f=0,g=0;
             if (d==0 || d<0)
             {
                 if (d=0){printf("0! = 1");}
