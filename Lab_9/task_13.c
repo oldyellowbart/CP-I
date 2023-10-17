47bae0af75da8e51e41a185e752fcf853ad2c226
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-17 14:58:50 +0500

    nothing

--- Lab_9/task_13.c
+++ Lab_9/task_13.c
@@ -8,32 +8,18 @@
   -------------------------------------------------------------------------------*/
   #include<stdio.h>
   void star(int,char);
-            void main()
-            {
-            int a=50;
-            char f='$';
-            char h='%';
-            star(a,f);
-            printf("Date\t\tNo\tSubject\n");
-            star(a,f);
-            printf("November,\t1\tVector and complex analysis\nNovember,\t2\tEngineering Dynamics\nNovember,\t3\tDigital logic design\nNovember,\t4\tComputer Programing-I\nNovember,\t5\tTranslation of Hly Quran-II\nNovember,\t6\tElectrical Machinery\n");
-            star(a+5,h);
-            }
-                    void star(int q,char r)
-                    {
-                        for (int i = 0; i < q; i++)
-                        {
-                            if (r=='$')
-                            {
-                                printf("$");
-                            }
-                            else if (r=='%')
-                            {
-                                printf("%%");
-                            }
-                            else{printf("Please Enter %% or $");}
-                            
-                            
-                        }
-                        printf("\n");
-                    }
+void main(){
+int a=50;
+char f='$';
+char h='%';
+star(a,f);
+printf("Date\t\tNo\tSubject\n");
+star(a,f);
+printf("November,\t1\tVector and complex analysis\nNovember,\t2\tEngineering Dynamics\nNovember,\t3\tDigital logic design\nNovember,\t4\tComputer Programing-I\nNovember,\t5\tTranslation of Hly Quran-II\nNovember,\t6\tElectrical Machinery\n");
+star(a+5,h);}
+void star(int q,char r){
+for (int i = 0; i < q; i++){
+if (r=='$'){printf("$");}
+else if (r=='%'){printf("%%");}
+else{printf("Please Enter %% or $");}}
+printf("\n");}
