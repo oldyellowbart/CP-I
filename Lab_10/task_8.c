2f5a4381f70efd50fac859a2113fe708a64e020e
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-24 14:31:39 +0500

    few

---
+++ Lab_10/task_8.c
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------------- 
+  Author        :  Haseeb-ul-hassan
+  Roll No       :  2022MC58
+  Dated         :  24/10/23
+  Purpose       :  Task task_8.c to get know about the nested loops
+                    and print matrix  till given number
+  Disclaimer    :  open source Code has been writen for educational purpose only 
+  -------------------------------------------------------------------------------*/
+#include<stdio.h>
+void test_scores(int);
+void main(){
+
+int a=0,b=0;
+}
