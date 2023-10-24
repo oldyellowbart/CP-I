49f0726e9ec5b8f75ee3ab1890d05501f173f3d0
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-24 15:00:55 +0500

    n

---
+++ Lab_10/task_9.c
@@ -0,0 +1,9 @@
+/*-------------------------------------------------------------------------------- 
+  Author        :  Haseeb-ul-hassan
+  Roll No       :  2022MC58
+  Dated         :  24/10/23
+  Purpose       :  Task task_9.c to get know about the nested loops
+                    and print sum of even and odd numbers  till given number
+  Disclaimer    :  open source Code has been writen for educational purpose only 
+  -------------------------------------------------------------------------------*/
+  
