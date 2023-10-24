608201193463be7534d0d31134a98197c20af144
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-10-24 14:58:54 +0500

    new

--- Lab_10/task_8.c
+++ Lab_10/task_8.c
@@ -7,8 +7,27 @@
   Disclaimer    :  open source Code has been writen for educational purpose only 
   -------------------------------------------------------------------------------*/
 #include<stdio.h>
-void test_scores(int);
+void test_scores(int,float);
 void main(){
 
 int a=0,b=0;
+printf("enter the number of students >>");
+scanf("%d",&a);
+printf("enter the number of subjects >> ");
+scanf("%d",&b);
+test_scores(a,b);
+}
+void test_scores(int q,float r){
+    int g=0;
+    
+for (int i = 0; i < q; i++)
+{int h=0;
+    for (int j = 0; j < r ; j++)
+    {
+        printf("Enter the %d subject marks of %d student >>",j+1,i+1);
+        scanf("%d",&g);
+        h=h+g;
+    }
+printf("The average of subjects is %.2f\n",h/r);
+}
 }
