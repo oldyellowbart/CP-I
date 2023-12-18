773dbcb1c96eff7a32f354d47044c0027419e6c6
xtylishmaher <ulhassanhaseeb@gmail.com>
2023-12-18 15:56:12 +0500

    c

--- eval/hasee.c
+++ eval/hasee.c
@@ -9,18 +9,69 @@ int main(){
 
 
 
-    int var[]={10,100,200};
-    for (int i = 0; i < 3; i++)
-    {
-         int *p=&var[i];
-         printf("\nthe adress of the value of %d is %p ",var[i],p);
-    }
+    // int var[]={10,100,200};
+    // int *p=&var[2];
+    // for (int i = 0; i < 3; i++)
+    // {   
+    //      printf("\nthe adress of the value of %d is %p ",*p,p);
+    //      p --;
+    // }
+    // printf("\n\n");
+    // for (int i = 0; i < 3; i++)
+    // {   
+    // printf("\nthe adress of the value of %d is %p ",var[i],&var[i]);
+    // }
+    // int *p = NULL;
+    // printf("%p",p);
+    // printf("\n%d",*p); // show null 7 element and sum using pointer
     
-    printf("\n\n");
-    for (int i = 0; i < 3; i++)
-    {   
-         printf("\nthe adress of the value of %d is %p ",var[i],&var[i]);
-    }
+
+
+    // int var[]={10,100,200};
+    // int (*p)[3]=&var;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("\n %p",p[i]);
+    //     printf("\n %d",(*p)[i]);
+    //     ;
+    // }
     
-   
+
+
+
+    // int array[7];
+    // int *p=array;
+    // int sum =0;
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     printf("Enter the %d value>> ",i+1);
+    //     scanf("%d",&array[i]);
+    //     sum = sum+*p;
+    //     p++;
+    // }
+    // printf("Sum is %d",sum);
+
+
+                            // char array[7];
+                            // char *ptr=&array[7];
+                            // printf("Enter word >> ");
+                            // scanf("%c",array);
+                            // for (int  i = 0; i < 7; i++)
+                            // {
+                            //     printf("%c",*ptr);
+                            //     ptr--;
+                            // }  
+
+
+
+
+                            int a=45;
+                            int *q=&a;
+                            int **r=&q;
+                            int ***s=&r;
+                            printf(" adress = %p_\t_value = %d",a,a);   
+                            printf(" \n adress = %p_\t_value = %d",q,*q);
+                            printf(" \n adress = %p_\t_value = %d",r,**r);
+                            printf(" \n adress = %p_\t_value = %d",s,***s);
+                               
 }
